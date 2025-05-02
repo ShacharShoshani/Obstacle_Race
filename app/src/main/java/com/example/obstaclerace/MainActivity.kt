c20c34c70215b34367f31685b8ddb3485c42d1ee
ShacharShoshani <145845453+ShacharShoshani@users.noreply.github.com>
2025-05-02 14:48:03 +0300

    UX: Make player object respond to left and right buttons

--- app/src/main/java/com/example/obstaclerace/MainActivity.kt
+++ app/src/main/java/com/example/obstaclerace/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.obstaclerace
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var main_IMG_hearts: Array<AppCompatImageView>
+    private lateinit var left_BTN: FloatingActionButton
+    private lateinit var right_BTN: FloatingActionButton
+    private lateinit var player: AppCompatImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        findViews()
+        initViews()
+    }
+
+    private fun initViews() {
+        left_BTN.setOnClickListener { view: View -> movePlayer(left = true) }
+        right_BTN.setOnClickListener { view: View -> movePlayer() }
+    }
+
+    private fun movePlayer(left: Boolean = false) {
+        if (left)
+            player.left += 1
+        else
+            player.right += 1
+    }
+
+    private fun findViews() {
+        left_BTN = findViewById(R.id.left_BTN)
+        right_BTN = findViewById(R.id.right_BTN)
+        player = findViewById(R.id.player_IMG)
+        main_IMG_hearts = arrayOf(
+            findViewById(R.id.main_IMG_heart0),
+            findViewById(R.id.main_IMG_heart1),
+            findViewById(R.id.main_IMG_heart2)
+        )
     }
 }
