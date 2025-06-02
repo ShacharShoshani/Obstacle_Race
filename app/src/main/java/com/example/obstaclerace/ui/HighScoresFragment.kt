1f1980928edf57f3fc12297572fa0a50003442bb
ShacharShoshani <145845453+ShacharShoshani@users.noreply.github.com>
2025-06-02 20:14:36 +0300

    Bugfix: Records list wasn't populating

--- app/src/main/java/com/example/obstaclerace/ui/HighScoresFragment.kt
+++ app/src/main/java/com/example/obstaclerace/ui/HighScoresFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.obstaclerace.R
 import com.example.obstaclerace.adapters.GameRecordAdapter
 import com.example.obstaclerace.interfaces.Callback_HighScoreClicked
@@ -14,8 +16,6 @@ import com.example.obstaclerace.utilities.DataManager
 
 
 class HighScoresFragment : Fragment() {
-    private val gameRecordAdapter = GameRecordAdapter(DataManager.getInstance().getTopRecords())
-
     var highScoreItemClicked: Callback_HighScoreClicked? = null
 
     override fun onCreateView(
@@ -23,12 +23,21 @@ class HighScoresFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val view: View = inflater.inflate(R.layout.fragment_high_scores, container, false)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.top_records_RV)
+
+        val gameRecordAdapter = GameRecordAdapter(DataManager.getInstance().getTopRecords())
+
         gameRecordAdapter.gameRecordCallback = object : GameRecordCallback {
             override fun itemClicked(record: GameRecord, position: Int) {
                 highScoreItemClicked?.highScoreItemClicked(record.locationLat, record.locationLon)
             }
 
         }
+
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.adapter = gameRecordAdapter
+
         return view
     }
 }
